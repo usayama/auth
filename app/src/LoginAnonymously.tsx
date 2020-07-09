39cda2459b83afeb39457a218939cac482013be9
y-nakane <y-nakane@sakura.ad.jp>
2020-07-09 22:05:42 +0900

    匿名ログインをモジュール化

--- app/src/LoginAnonymously.tsx
+++ app/src/LoginAnonymously.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { auth } from './firebase'
 
+export const loginAnonymously = () => {
+  auth.signInAnonymously().catch(error => {
+    console.log(error.code)
+    console.log(error.message)
+  })
+}
+
 export const LoginAnonymously: React.FC = () => {
-  const loginAnonymously = () => {
-    auth.signInAnonymously().catch(error => {
-      console.log(error.code)
-      console.log(error.message)
-    })
-  }
   return <button onClick={loginAnonymously}>匿名ログイン</button>
 }
