0fc20ba6dce589e0c00a94bccf1cf98b32616077
y-nakane <y-nakane@sakura.ad.jp>
2020-07-09 22:01:38 +0900

    ログアウトをカスタムフック化

--- app/src/Logout.tsx
+++ app/src/Logout.tsx
@@ -2,25 +2,23 @@ import React, { useContext } from 'react'
 import { auth } from './firebase'
 import { UserContext } from 'contexts'
 
-const useSignOut = (user: any) => {
-  // 匿名ユーザーは削除してサインアウト
-  if (user && user.isAnonymous) {
-    auth.currentUser?.delete().catch(error => {
-      console.log(error.code)
-      console.log(error.message)
-    })
-    return true
-  } else {
-    // 他は普通にサインアウト
-    auth.signOut()
-    return true
+export const useLogout = () => {
+  const user = useContext(UserContext).user
+
+  return () => {
+    if (user?.isAnonymous) {
+      auth.currentUser?.delete().catch(error => {
+        console.log(error.log)
+        console.log(error.message)
+      })
+    } else {
+      auth.signOut()
+    }
   }
 }
 
 const Logout: React.FC = () => {
-  const user = useContext(UserContext).user
-  const signOut = useSignOut(user)
-  return <button onClick={signOut}>ログアウト</button>
+  return <button onClick={useLogout()}>ログアウト</button>
 }
 
 export default Logout
